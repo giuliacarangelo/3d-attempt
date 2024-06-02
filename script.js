dd20f70e04ce4e4bc4a5f5b2183de88282e12a3e
GiuliaCarangelo <tnhkagura@gmail.com>
2024-06-02 12:25:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app
+    .load('https://prod.spline.design/V2r4JRymvcW91HRT/scene.splinecode')
+    .then(() => {
+        const carz = app.findObjectByName("carz");
+    }
+)
